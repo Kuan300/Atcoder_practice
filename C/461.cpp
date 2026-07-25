173062ccd29b75ac0aa0b969e46c8c76ae177d84
chinkuan300@gmail.com <chinkuan300@gmail.com>
2026-07-25 13:35:23 +0800

    fix: 完成461C

--- C/461.cpp
+++ C/461.cpp
@@ -24,7 +24,7 @@ int main(){
     vector<long long> max_per_color;
     vector<long long> remaining_color;
 
-    for (int i=0; i<n; i++){
+    for (int i=1; i<=n; i++){
         if (color_gems[i].empty())
             continue;
         
@@ -34,17 +34,30 @@ int main(){
         max_per_color.push_back(color_gems[i][0]);
 
         // 該顏色剩下的寶石放進剩餘池
-        for (int j=1; j<color_gems[i].size(); i++){
+        for (size_t j=1; j<color_gems[i].size(); j++){
             remaining_color.push_back(color_gems[i][j]);
         }
     }
 
     // 1. 先處理 mandatory 部分：取前 M 大不同顏色的代表
+    sort(max_per_color.rbegin(), max_per_color.rend());
 
+    long long count = 0;
+    for (int i=0; i<m; i++){
+        count += max_per_color[i];
+    }
 
     // 2. 把沒選到的顏色代表放回剩餘池，一起競爭剩下的 K - M 個名額
-
+    for (size_t i=m; i<max_per_color.size(); i++){
+        remaining_color.push_back(max_per_color[i]);
+    }
 
     // 3. 貪婪選剩下的 K - M 顆最大值
+    sort(remaining_color.rbegin(), remaining_color.rend());
+
+    for (int i=0; i<(k-m); i++){
+        count += remaining_color[i];
+    }
     
+    cout << count;
 }
